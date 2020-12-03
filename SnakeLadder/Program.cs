ca524f26251d936f048983d292da8fdb3765f470
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-03 22:35:23 +0530

    [Pratibha Mastud] Add: Game Start with Single player

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static int startPosition = 0;
+        static int topPosition = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------Welcome to Snake and ladder game----------------------");
+            Console.WriteLine("The game Played with single Player at the start position is : {0}", startPosition);
+
+        }
+    }
+}
